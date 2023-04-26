67a7fb8cda9c09fa2a305ab7fa63b7ac08aab452
ArseniyKruglov <arseniy.krugloff@gmail.com>
2023-04-26 23:01:04 +0300

    Улучшено форматирование

--- I семестр/Лабораторная работа №1/Исходный код.pas
+++ I семестр/Лабораторная работа №1/Исходный код.pas
@@ -1,21 +1,20 @@
-﻿Program Log_Oper;
-{ Выполнение поразрядных логических операций над целыми числами }
+﻿{ Выполнение поразрядных логических операций над целыми числами }
 
 Var
-  answer: Byte;
+	Answer: Byte;
 
 Begin
-  answer := 109 AND 18;
-  WriteLn('109 AND 18 = ', answer, ' (контр. значение 0)');
+	Answer := 109 AND 18;
+	WriteLn('109 AND 18 = ', Answer, ' (контр. значение 0)');
 
-  answer := 109 OR -18;
-  WriteLn('109 OR -18 = ', answer, ' (контр. значение 239)');
+	Answer := 109 OR -18;
+	WriteLn('109 OR -18 = ', Answer, ' (контр. значение 239)');
 
-  answer := 109 SHR 3;
-  WriteLn('109 >> 3 = ', answer, ' (контр. значение 13)');
+	Answer := 109 SHR 3;
+	WriteLn('109 >> 3 = ', Answer, ' (контр. значение 13)');
 
-  answer := 109 OR NOT -18 AND (NOT 58 XOR -65);
-  WriteLn('109 OR NOT -18 AND (NOT 58 XOR -65) = ', answer, ' (контр. знач. 125)');
+	Answer := 109 OR NOT -18 AND (NOT 58 XOR -65);
+	WriteLn('109 OR NOT -18 AND (NOT 58 XOR -65) = ', Answer, ' (контр. знач. 125)');
 
-  ReadLn;
+	ReadLn;
 End.
