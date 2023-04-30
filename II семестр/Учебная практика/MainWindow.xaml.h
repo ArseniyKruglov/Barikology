c7c8e7f1461f8606833a51646551c06a8a0a8bb0
ArseniyKruglov <arseniy.krugloff@gmail.com>
2023-05-01 00:31:39 +0300

    Улучшено форматирование

--- II семестр/Учебная практика/MainWindow.xaml.h
+++ II семестр/Учебная практика/MainWindow.xaml.h
@@ -13,44 +13,44 @@ typedef TElement TMatrix[MaxSize][MaxSize];
 
 namespace winrt::WinUI::implementation
 {
-    struct MainWindow : MainWindowT<MainWindow>
-    {
-        MainWindow();
+	struct MainWindow : MainWindowT<MainWindow>
+	{
+		MainWindow();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
+		int32_t MyProperty();
+		void MyProperty(int32_t value);
 
 
 
-        // Variables
-        int n;
-        TMatrix Matrix_Input;
-        TMatrix Matrix_Output;
+		// Variables
+		int n;
+		TMatrix Matrix_Input;
+		TMatrix Matrix_Output;
 
-        // Main
-        void Input_Matrix_DrawEmpty();
-        void Sort();
+		// Main
+		void Input_Matrix_DrawEmpty();
+		void Sort();
 
-        // Handlers
-        void Slider_ValueChanged(Windows::Foundation::IInspectable const& Sender, Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& Arguments);
-        void NumberBox_ValueChange(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::Controls::NumberBoxValueChangedEventArgs const& Arguments);
+		// Handlers
+		void Slider_ValueChanged(Windows::Foundation::IInspectable const& Sender, Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& Arguments);
+		void NumberBox_ValueChange(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::Controls::NumberBoxValueChangedEventArgs const& Arguments);
 
-        // Helpers
-        void Input_Matrix_Fill(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& Arguments);
-        void Input_Matrix_Empty(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& Arguments);
+		// Helpers
+		void Input_Matrix_Fill(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& Arguments);
+		void Input_Matrix_Empty(winrt::Windows::Foundation::IInspectable const& Sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& Arguments);
 
-        // Output
-        void Output_Error();
-        void Output_Matrix_DrawEmpty();
-        void Output_Matrix_Fill();
-        void Output_Success();
-        void Input_UpdateNValue();
-    };
+		// Output
+		void Output_Error();
+		void Output_Matrix_DrawEmpty();
+		void Output_Matrix_Fill();
+		void Output_Success();
+		void Input_UpdateNValue();
+	};
 }
 
 namespace winrt::WinUI::factory_implementation
 {
-    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
-    {
-    };
+	struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+	{
+	};
 }
